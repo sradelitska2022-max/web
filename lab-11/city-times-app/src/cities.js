0a182003c0fa919fe37fd68a7c42c6a10d290eae
Sofia <sradelitska2022@itcollege.lviv.ua>
2025-12-18 17:56:19 +0200

    laboran11

---
+++ lab-11/city-times-app/src/cities.js
@@ -0,0 +1,32 @@
+import { 
+  uk, 
+  enGB, 
+  enUS, 
+  ja, 
+  fr, 
+  de, 
+  ptBR, 
+  tr, 
+  ar,
+  es,
+  it
+} from 'date-fns/locale';
+
+export const cities = [
+  { name: 'Lviv', zone: 'Europe/Kyiv', locale: uk },
+  { name: 'London', zone: 'Europe/London', locale: enGB },
+  { name: 'New York', zone: 'America/New_York', locale: enUS },
+  { name: 'Tokyo', zone: 'Asia/Tokyo', locale: ja },
+  { name: 'Paris', zone: 'Europe/Paris', locale: fr },
+  { name: 'Berlin', zone: 'Europe/Berlin', locale: de },
+  { name: 'Sydney', zone: 'Australia/Sydney', locale: enGB },
+  { name: 'Rio de Janeiro', zone: 'America/Sao_Paulo', locale: ptBR },
+  { name: 'Istanbul', zone: 'Europe/Istanbul', locale: tr },
+  { name: 'Dubai', zone: 'Asia/Dubai', locale: ar },
+  { name: 'Madrid', zone: 'Europe/Madrid', locale: es },
+  { name: 'Buenos Aires', zone: 'America/Argentina/Buenos_Aires', locale: es },
+  { name: 'Cairo', zone: 'Africa/Cairo', locale: ar },
+{ name: 'Chicago', zone: 'America/Chicago', locale: enUS },
+{ name: 'Lisbon', zone: 'Europe/Lisbon', locale: ptBR },
+
+];
